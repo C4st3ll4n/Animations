74b67868771cc1c463c5f4ceecadfe989be78173
Pedro Henrique <p13tr0@outlook.com>
2018-05-08 10:36:43 -0300

    Initial commit

---
+++ app/src/main/java/m/castellan/animations/MainActivity.java
@@ -0,0 +1,46 @@
+package m.castellan.animations;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+import android.transition.Scene;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ViewGroup root;
+    private ImageView img;
+    private Scene cena1, cena2;
+    private boolean cena = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        root = findViewById(R.id.root);
+        cena1 = Scene.getSceneForLayout(root, R.layout.activity_main, this);
+        cena2 = Scene.getSceneForLayout(root, R.layout.activity_main2, this);
+        img = findViewById(R.id.imageView);
+
+    }
+
+    public void click(View v) {
+        TransitionManager.beginDelayedTransition(root, new Explode());
+        img.setVisibility((img.getVisibility() == View.VISIBLE) ? View.INVISIBLE : View.VISIBLE);
+    }
+
+    public void trocaCena(View v) {
+        Transition trans = TransitionInflater.from(this).inflateTransition(R.transition.custom);
+        if (cena) {
+            TransitionManager.go(cena2,trans);
+        } else {
+            TransitionManager.go(cena1, trans);
+        }
+        cena = !cena;
+    }
+}
